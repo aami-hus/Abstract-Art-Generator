492f268cd38b600b799d812f7361ee07286b08df
Fady Morcos <fadymorcos22@gmail.com>
2022-03-17 21:37:13 -0400

    defined imports

---
+++ src/modules/overlay.py
@@ -0,0 +1,17 @@
+##
+# @file overlay.py
+#
+# @brief Defines the overlay class.
+#
+# @section author_sensors Author(s)
+# - Created by Fady Morcos on 03/17/2022.
+
+# Imports
+from random import randint, choice
+
+import pygame_gui as pgui
+import pygame as pg
+
+from modules.widget import widget
+import assets
+
