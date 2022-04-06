a7d336f6c20711c7e0974463b6310213d82c8e4d
aami-hus <hussaa54@mcmaster.ca>
2022-04-05 22:15:09 -0400

    added new border options to overlay module

--- src/modules/overlay.py
+++ src/modules/overlay.py
@@ -21,7 +21,10 @@ _overlays = [
     "assets/overlay3.png",
     "assets/overlay4.png",
     "assets/overlay5.png",
-    "assets/overlay6.png"
+    "assets/overlay6.png",
+    "assets/border7.png",
+    "assets/border8.png",
+    "assets/border9.png"
 ]
 
 class overlay(widget):
@@ -66,20 +69,27 @@ class overlay(widget):
 
         assets.text_to_screen(window=self.__window, text="OVERLAY/BORDER", color=self.ui_h1_color, pos=(self.__x+15, self.__y+12), font_size=18)
 
-        pg.draw.rect(self.__window, self.__active_color if self.__active_overlay == 1 else self.__inactive_color, (self.__x+13, self.__y+38, 84, 49), 1)
-        pg.draw.rect(self.__window, self.__active_color if self.__active_overlay == 2 else self.__inactive_color, (self.__x+113, self.__y+38, 84, 49), 1)
-        pg.draw.rect(self.__window, self.__active_color if self.__active_overlay == 3 else self.__inactive_color, (self.__x+13, self.__y+118, 84, 49), 1)
-        pg.draw.rect(self.__window, self.__active_color if self.__active_overlay == 4 else self.__inactive_color, (self.__x+113, self.__y+118, 84, 49), 1)
-        pg.draw.rect(self.__window, self.__active_color if self.__active_overlay == 5 else self.__inactive_color, (self.__x+13, self.__y+198, 84, 49), 1)
-        pg.draw.rect(self.__window, self.__active_color if self.__active_overlay == 6 else self.__inactive_color, (self.__x+113, self.__y+198, 84, 49), 1)
-        pg.draw.rect(self.__window, self.__active_color if self.__active_overlay == 0 else self.__inactive_color, (self.__x+63, self.__y+278, 84, 49), 1)
+        pg.draw.rect(self.__window, self.__active_color if self.__active_overlay == 1 else self.__inactive_color, (self.__x+13+20, self.__y+33, 84, 49), 1)
+        pg.draw.rect(self.__window, self.__active_color if self.__active_overlay == 2 else self.__inactive_color, (self.__x+113+40, self.__y+33, 84, 49), 1)
+        pg.draw.rect(self.__window, self.__active_color if self.__active_overlay == 3 else self.__inactive_color, (self.__x+13+20, self.__y+98, 84, 49), 1)
+        pg.draw.rect(self.__window, self.__active_color if self.__active_overlay == 4 else self.__inactive_color, (self.__x+113+40, self.__y+98, 84, 49), 1)
+        pg.draw.rect(self.__window, self.__active_color if self.__active_overlay == 5 else self.__inactive_color, (self.__x+13+20, self.__y+163, 84, 49), 1)
+        pg.draw.rect(self.__window, self.__active_color if self.__active_overlay == 6 else self.__inactive_color, (self.__x+113+40, self.__y+163, 84, 49), 1)
+        pg.draw.rect(self.__window, self.__active_color if self.__active_overlay == 7 else self.__inactive_color, (self.__x+13+20, self.__y+228, 84, 49), 1)
+        pg.draw.rect(self.__window, self.__active_color if self.__active_overlay == 8 else self.__inactive_color, (self.__x+113+40, self.__y+228, 84, 49), 1)
+        pg.draw.rect(self.__window, self.__active_color if self.__active_overlay == 9 else self.__inactive_color, (self.__x+13+20, self.__y+293, 84, 49), 1)
+        pg.draw.rect(self.__window, self.__active_color if self.__active_overlay == 0 else self.__inactive_color, (self.__x+113+40, self.__y+293, 84, 49), 1)
+        #pg.draw.rect(self.__window, self.__active_color, (self.__x+63, self.__y+293, 84, 49), 1)
 
-        self.__window.blit(pg.transform.scale(self.__overlays[0], (80, 45)), (self.__x+15, self.__y+40))
-        self.__window.blit(pg.transform.scale(self.__overlays[1], (80, 45)), (self.__x+115, self.__y+40))
-        self.__window.blit(pg.transform.scale(self.__overlays[2], (80, 45)), (self.__x+15, self.__y+120))
-        self.__window.blit(pg.transform.scale(self.__overlays[3], (80, 45)), (self.__x+115, self.__y+120))
-        self.__window.blit(pg.transform.scale(self.__overlays[4], (80, 45)), (self.__x+15, self.__y+200))
-        self.__window.blit(pg.transform.scale(self.__overlays[5], (80, 45)), (self.__x+115, self.__y+200))
+        self.__window.blit(pg.transform.scale(self.__overlays[0], (80, 45)), (self.__x+15+20, self.__y+35))
+        self.__window.blit(pg.transform.scale(self.__overlays[1], (80, 45)), (self.__x+115+40, self.__y+35))
+        self.__window.blit(pg.transform.scale(self.__overlays[2], (80, 45)), (self.__x+15+20, self.__y+100))
+        self.__window.blit(pg.transform.scale(self.__overlays[3], (80, 45)), (self.__x+115+40, self.__y+100))
+        self.__window.blit(pg.transform.scale(self.__overlays[4], (80, 45)), (self.__x+15+20, self.__y+165))
+        self.__window.blit(pg.transform.scale(self.__overlays[5], (80, 45)), (self.__x+115+40, self.__y+165))
+        self.__window.blit(pg.transform.scale(self.__overlays[6], (80, 45)), (self.__x+15+20, self.__y+230))
+        self.__window.blit(pg.transform.scale(self.__overlays[7], (80, 45)), (self.__x+115+40, self.__y+230))
+        self.__window.blit(pg.transform.scale(self.__overlays[8], (80, 45)), (self.__x+15+20, self.__y+295))
 
 
 
@@ -93,20 +103,26 @@ class overlay(widget):
         Draws the overlay selection buttons.
         """
 
-        overlay1_button = pgui.elements.UIButton(relative_rect=pg.Rect(self.__x+48, self.__y+90, 14, 14), text="", manager=self.__ui_manager,
+        overlay1_button = pgui.elements.UIButton(relative_rect=pg.Rect(self.__x+15, self.__y+55.5, 14, 14), text="", manager=self.__ui_manager,
                                              object_id="overlay1_button")
-        overlay2_button = pgui.elements.UIButton(relative_rect=pg.Rect(self.__x+148, self.__y+90, 14, 14), text="", manager=self.__ui_manager,
+        overlay2_button = pgui.elements.UIButton(relative_rect=pg.Rect(self.__x+135, self.__y+55.5, 14, 14), text="", manager=self.__ui_manager,
                                                 object_id="overlay2_button")
-        overlay3_button = pgui.elements.UIButton(relative_rect=pg.Rect(self.__x+48, self.__y+170, 14, 14), text="", manager=self.__ui_manager,
+        overlay3_button = pgui.elements.UIButton(relative_rect=pg.Rect(self.__x+15, self.__y+120.5, 14, 14), text="", manager=self.__ui_manager,
                                                 object_id="overlay3_button")
-        overlay4_button = pgui.elements.UIButton(relative_rect=pg.Rect(self.__x+148, self.__y+170, 14, 14), text="", manager=self.__ui_manager,
+        overlay4_button = pgui.elements.UIButton(relative_rect=pg.Rect(self.__x+135, self.__y+120.5, 14, 14), text="", manager=self.__ui_manager,
                                                 object_id="overlay4_button")
-        overlay5_button = pgui.elements.UIButton(relative_rect=pg.Rect(self.__x+48, self.__y+250, 14, 14), text="", manager=self.__ui_manager,
+        overlay5_button = pgui.elements.UIButton(relative_rect=pg.Rect(self.__x+15, self.__y+185.5, 14, 14), text="", manager=self.__ui_manager,
                                                 object_id="overlay5_button")
-        overlay6_button = pgui.elements.UIButton(relative_rect=pg.Rect(self.__x+148, self.__y+250, 14, 14), text="", manager=self.__ui_manager,
+        overlay6_button = pgui.elements.UIButton(relative_rect=pg.Rect(self.__x+135, self.__y+185.5, 14, 14), text="", manager=self.__ui_manager,
                                                 object_id="overlay6_button")
-        overlay7_button = pgui.elements.UIButton(relative_rect=pg.Rect(self.__x+98, self.__y+330, 14, 14), text="", manager=self.__ui_manager,
-                                                object_id="overlay7_button")
+        border7_button = pgui.elements.UIButton(relative_rect=pg.Rect(self.__x+15, self.__y+250.5, 14, 14), text="", manager=self.__ui_manager,
+                                                object_id="border7_button")
+        border8_button = pgui.elements.UIButton(relative_rect=pg.Rect(self.__x+135, self.__y+250.5, 14, 14), text="", manager=self.__ui_manager,
+                                                object_id="border8_button")
+        border9_button = pgui.elements.UIButton(relative_rect=pg.Rect(self.__x+15, self.__y+315.5, 14, 14), text="", manager=self.__ui_manager,
+                                                object_id="border9_button")
+        no_overlay_button = pgui.elements.UIButton(relative_rect=pg.Rect(self.__x+135, self.__y+315.5, 14, 14), text="", manager=self.__ui_manager,
+                                                object_id="no_overlay_button")
 
 
     def events(self, event):
@@ -135,7 +151,16 @@ class overlay(widget):
             if event.ui_object_id == "overlay6_button":
                 self.__active_overlay = 6
                 self.draw_canvas()
-            if event.ui_object_id == "overlay7_button":
+            if event.ui_object_id == "border7_button":
+                self.__active_overlay = 7
+                self.draw_canvas()
+            if event.ui_object_id == "border8_button":
+                self.__active_overlay = 8
+                self.draw_canvas()
+            if event.ui_object_id == "border9_button":
+                self.__active_overlay = 9
+                self.draw_canvas()
+            if event.ui_object_id == "no_overlay_button":
                 self.__active_overlay = 0
                 self.clean_layer()
 
